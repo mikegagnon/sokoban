fff103518cd0704e57c27a427ab58e09d0e8095e
Michael Gagnon <mikegagnon@gmail.com>
2016-12-27 15:27:37 -0500

    Cell class

--- sokoban.js
+++ sokoban.js
@@ -1,14 +1,15 @@
-var numRows = 10;
-var numCols = 10;
+var numRows = 8;
+var numCols = 8;
 
 cellSize = 50;
 
+EMPTY = 0;
 BLOCK = 1;
 SLIDER = 2;
 PLAYER = 3;
 GOAL = 4;
 
-var matrix = [
+var boardInit = [
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 1, 0, 0, 0, 0, 4],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -19,6 +20,67 @@ var matrix = [
     [0, 3, 0, 0, 0, 0, 2, 0],
 ];
 
+var matrix = undefined;
+
+class Cell {
+    constructor() {
+        this.block = false;
+        this.slider = false;
+        this.player = false;
+        this.goal = false;
+    }
+
+    addPiece(pieceId) {
+        if (pieceId == BLOCK) {
+            this.block = true;
+        } else if (pieceId == SLIDER) {
+            this.slider = true;
+        } else if (pieceId == PLAYER) {
+            this.player = true;
+        } else if (pieceId == GOAL) {
+            this.goal = true;
+        } else {
+            console.error("Unrecognized pieceId: " + pieceId);
+        }
+    }
+
+    removePiece(pieceId) {
+        if (pieceId == BLOCK) {
+            this.block = false;
+        } else if (pieceId == SLIDER) {
+            this.slider = false;
+        } else if (pieceId == PLAYER) {
+            this.player = false;
+        } else if (pieceId == Goal) {
+            this.goal = false;
+        } else {
+            console.error("Unrecognized pieceId: " + pieceId);
+        }
+    }
+
+}
+
+
+function initMatrix() {
+    matrix = new Array(numRows);
+
+    for (var row = 0; row < numRows; row++) {
+
+        matrix[row] = new Array(numCols);
+
+        for (var col = 0; col < numCols; col++) {
+            var pieceId = boardInit[row][col];
+            var cell = new Cell();
+
+            if (pieceId != EMPTY) {
+                cell.addPiece(pieceId);
+            }
+
+            matrix[row][col] = cell;
+        }
+    }
+}
+
 
 function getCellId(row, col) {
     return "cell-" + row + "-" + col;
@@ -27,18 +89,18 @@ function getCellId(row, col) {
 function drawGame() {
     for (var row = 0; row < numRows; row++) {
         for (var col = 0; col < numCols; col++) {
-            var cellValue = matrix[row][col];
+            var cell = matrix[row][col];
             var cellId = "#" + getCellId(row, col);
 
             var src = undefined;
 
-            if (cellValue == BLOCK) {
+            if (cell.block) {
                 src = "block.png";
-            } else if (cellValue == SLIDER) {
+            } else if (cell.slider) {
                 src = "slider.png";
-            } else if (cellValue == PLAYER) {
+            } else if (cell.player) {
                 src = "player.png"
-            } else if (cellValue == GOAL) {
+            } else if (cell.goal) {
                 src = "goal.png"
             }
 
@@ -69,5 +131,6 @@ function createSokoban(boardId) {
         }
     }
 
+    initMatrix();
     drawGame();
 }
