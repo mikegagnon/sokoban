822f4588c9abf9d1fba8f4312d2e444bc514e009
Michael Gagnon <mikegagnon@gmail.com>
2017-01-03 20:07:48 -0500

    C2-7 sokoban

--- challenge2-7/sokoban.js
+++ challenge2-7/sokoban.js
@@ -292,11 +292,45 @@ class Sokoban {
         }
     }
 
+    checkForVictory() {
+
+        var numGoals = 0;
+        var numOccupiedGoals = 0;
+
+        for (var row = 0; row < this.board.numRows; row++) {
+            for (var col = 0; col < this.board.numCols; col++) {
+                var cell = this.board.cells[row][col];
+                if (cell.goal) {
+                    numGoals += 1;
+
+                    if (cell.slider) {
+                        numOccupiedGoals += 1;
+                    }
+                }
+            }
+        }
+
+        if (numGoals == 0) {
+            return false;
+        } else {
+            return numGoals == numOccupiedGoals;
+        }
+
+    }
+
     // Moves the player in the specified direction. direction must be either:
     // "up", "down", "left", or "right"
     // Returns a snapshot object that defines the game state after the player is moved
     move(direction) {
+
+        if (this.board.gameOver) {
+            return IsoSnapshotBoard.toSnapshot(this.board); 
+        }
+
         this.push(this.playerRow, this.playerCol, direction);
+
+        this.board.gameOver = this.checkForVictory();
+
         return IsoSnapshotBoard.toSnapshot(this.board);
     }
 }
@@ -599,7 +633,7 @@ assert(snapshots_equal(snapshot_result, snapshot_expected));
 /* Test push 1 crate over goal ************************************************/
 
 var matrix = [
-    [0, 0],
+    [0, 4],
     [4, 0],
     [2, 0],
     [3, 0],
@@ -608,7 +642,7 @@ var snapshot_init = new Snapshot(matrix, false);
 var sokoban = new Sokoban(snapshot_init);
 var snapshot_result = sokoban.move("up");
 var matrix_expected = [
-    [0, 0],
+    [0, 4],
     [5, 0],
     [3, 0],
     [0, 0],
